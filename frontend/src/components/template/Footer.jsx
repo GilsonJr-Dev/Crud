2b8b0a1773917ec3698612e61e8937b7dcdb3418
GilsonJr-Dev <gilsonmendes.jr@hotmail.com>
2019-03-16 14:58:47 -0300

    First commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    Desenvolvido por <strong>Gils<span className="text-danger">o</span>n Jr.</strong>
+</footer>
